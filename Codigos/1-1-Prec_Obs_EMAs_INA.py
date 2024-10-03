1301e2900bc05acd1d69691fc95aafb8597ec34c
marinalagos <mlmarinalagos@gmail.com>
2024-10-03 16:51:55 -0300

    inicio codigo EMAs

--- Codigos/1-1-Prec_Obs_EMAs_INA.py
+++ Codigos/1-1-Prec_Obs_EMAs_INA.py
@@ -1,2 +1,16 @@
 import pandas as pd
 import requests
+import json
+
+# CONSULTA A LA BASE DEL INA
+# Obtener token
+
+with open('credenciales.json', 'r') as f:
+    content = f.read()
+    if not content.strip():
+        print("El archivo credenciales.json está vacío.")
+    else:
+        token_base_INA = json.loads(content)['token_base_INA']
+
+# Obtener listado de EMAs a consultar
+
