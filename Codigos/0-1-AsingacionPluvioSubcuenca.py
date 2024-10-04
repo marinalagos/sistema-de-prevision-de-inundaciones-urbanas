c62534f2e12589794d7680cf4ad9f62bc4c82d15
marinalagos <mlmarinalagos@gmail.com>
2024-10-04 13:51:33 -0300

    agregar modificacion de inp (incompleto)

--- Codigos/0-1-AsingacionPluvioSubcuenca.py
+++ Codigos/0-1-AsingacionPluvioSubcuenca.py
@@ -25,17 +25,10 @@ posgar2geo = pyproj.Transformer.from_crs(crs_SWMM, crs_netCDF, always_xy=True) #
 
 
 
-# 1. EXTRACCIÓN DE CENTROIDES DE LAS SUBCUENCAS A PARTIR DEL .inp
+# 1. EXTRACCIÓN DE CENTROIDES DE LOS POLÍGONOS DE SUBCUENCAS A PARTIR DEL .inp
 # Abre el archivo de entrada y lee todas las líneas
 with open(inp_file) as f:
     lines = f.readlines()
-
-# Extracción de la tabla de subcuencas del archivo .inp
-inicio = lines.index('[SUBCATCHMENTS]\n') + 3
-fin = lines.index('[SUBAREAS]\n') - 1
-lines_subcuencas = lines[inicio:fin]
-df_subcuencas = pd.DataFrame([i.split() for i in lines_subcuencas], columns=['Name', 'RainGage', 'Outlet', 'Area', 'Imperv', 'Width', 'Slope', 'CurbLen'])
-df_subcuencas = df_subcuencas.set_index('Name')
 
 # Extracción de la tabla de polígonos del archivo .inp
 ini = lines.index('[Polygons]\n') + 3
@@ -116,3 +109,27 @@ for clave1, coordenadas1 in zip(subc_centroid.keys(), coordenadas1):
 
 # Crea un DataFrame a partir del diccionario de asignación
 df_sub_cell = pd.DataFrame.from_dict(asignacion, orient='index')
+
+
+
+# MODIFICACIÓN DE TABLA DE SUBCUENCAS EN EL ARCHIVO .inp
+# Extracción de la tabla de subcuencas del archivo .inp
+inicio = lines.index('[SUBCATCHMENTS]\n') + 3
+fin = lines.index('[SUBAREAS]\n') - 1
+lines_subcuencas = lines[inicio:fin]
+df_subcuencas = pd.DataFrame([i.split() for i in lines_subcuencas], columns=['Name', 'RainGage', 'Outlet', 'Area', 'Imperv', 'Width', 'Slope', 'CurbLen'])
+df_subcuencas = df_subcuencas.set_index('Name')
+
+# Cruzar con el dataframe de asignación subcuenca-celda
+df_subcuencas = pd.merge(df_subcuencas, df_sub_cell,
+                        right_index = True, left_index = True,
+                        how = 'left', sort = False)
+
+df_subcuencas[0] = df_subcuencas[0].fillna(df_sub_cell.values[0][0]) # Para las celdas sin área, no hay celda asociada. En esos casos, se asigna una celda arbitraria (la primera de la lista)
+df_subcuencas['RainGage'] = df_subcuencas[0]
+df_subcuencas = df_subcuencas.drop(columns=[0])
+
+str_subcatchments = df_subcuencas.to_string(index=True, header=False, index_names=False, justify='left')
+# inicio = lines2.index('[SUBCATCHMENTS]\n') + 3
+# fin = lines2.index('[SUBAREAS]\n') -1
+# lines3 = lines2[:inicio] + [str_subcatchments] + ['\n'] + lines2[fin:]
