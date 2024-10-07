b07d77e622c5d7dc5168ccc002f80ba4fedb32a2
marinalagos <mlmarinalagos@gmail.com>
2024-10-07 13:25:42 -0300

    preprocesamiento datos EMAs

--- Codigos/1-1-Prec_Obs_EMAs_INA.py
+++ Codigos/1-1-Prec_Obs_EMAs_INA.py
@@ -11,33 +11,37 @@ if repo_path:
 
 
 
-# 1. PARSEAR INPUTS
-# Crear el parser
-parser = argparse.ArgumentParser(description='Procesar archivos de entrada y NetCDF.')
+# # 1. PARSEAR INPUTS
+# # Crear el parser
+# parser = argparse.ArgumentParser(description='Procesar archivos de entrada y NetCDF.')
 
-# Añadir los argumentos que esperas recibir
-parser.add_argument('inicio_sim', 
-                    type = str, 
-                    help = 'Timestamp (UTC) del inicio de la simulación. Formato: yyyy-mm-dd hh:mm'
-                    )
+# # Añadir los argumentos que esperas recibir
+# parser.add_argument('inicio_sim', 
+#                     type = str, 
+#                     help = 'Timestamp (UTC) del inicio de la simulación. Formato: yyyy-mm-dd hh:mm'
+#                     )
 
-parser.add_argument('fin_sim', 
-                    type = str, 
-                    help = 'Timestamp (UTC) del fin de la simulación. Formato: yyyy-mm-dd hh:mm'
-                    )
+# parser.add_argument('fin_sim', 
+#                     type = str, 
+#                     help = 'Timestamp (UTC) del fin de la simulación. Formato: yyyy-mm-dd hh:mm'
+#                     )
 
-parser.add_argument('--cell_coords', 
-                    type = str, 
-                    help = 'Ruta al .csv con las coordenadas de la grilla.',
-                    default = 'Carpeta_base_SWMM/coordenadas_celdas.csv'
-                    )
+# parser.add_argument('--cell_coords', 
+#                     type = str, 
+#                     help = 'Ruta al .csv con las coordenadas de la grilla.',
+#                     default = 'Carpeta_base_SWMM/coordenadas_celdas.csv'
+#                     )
 
-# Parsear los argumentos
-args = parser.parse_args()
+# # Parsear los argumentos
+# args = parser.parse_args()
 
-inicio_sim = pd.to_datetime(args.inicio_sim, utc=True)
-fin_sim = pd.to_datetime(args.fin_sim, utc=True)
-path_cell_cords = args.cell_coords
+# inicio_sim = pd.to_datetime(args.inicio_sim, utc=True)
+# fin_sim = pd.to_datetime(args.fin_sim, utc=True)
+# path_cell_cords = args.cell_coords
+
+inicio_sim = pd.to_datetime('2024-10-01 00:00', utc=True)
+fin_sim = pd.to_datetime('2024-10-01 01:00', utc=True)
+path_cell_cords = 'Carpeta_base_SWMM/coordenadas_celdas.csv'
 
                     
 
@@ -61,13 +65,43 @@ with open('config.json', 'r') as f:
 
 
 # 3. CONSULTA A LA BASE DEL INA
-ids_serie = params_base_INA['ids_EMAs_consultadas']    
+ids_serie = params_base_INA['ids_EMAs_consultadas']
+
+dfs = []
 
 for id_serie in ids_serie:
     print(id_serie)
     response = requests.get(
     f"https://alerta.ina.gob.ar/a6/obs/puntual/series/{id_serie}/observaciones", 
     headers={'Authorization': f'Bearer {token_base_INA}'}, 
-    params={'timestart': '2024-09-13', 'timeend': '2024-09-15'}
+    # 'timestart' y 'timeend' en hora BA (+03)
+    params={'timestart': f'{inicio_sim.tz_convert(tz="America/Argentina/Buenos_Aires"):%Y-%m-%d %H:%M}', 
+            'timeend': f'{fin_sim.tz_convert(tz="America/Argentina/Buenos_Aires"):%Y-%m-%d %H:%M}'}
     )
-    print(response.json())
+    df = pd.DataFrame(response.json())
+    if not df.empty:
+        df = df.rename(columns={'valor': id_serie})
+        df = df[['timestart', 'timeend', id_serie]]
+        df.timestart = pd.to_datetime(df.timestart)
+        df.timeend = pd.to_datetime(df.timeend)
+        dfs.append(df)
+        j = response.json()
+        print(df.timestart - df.timeend)
+
+
+# 4. PREPROCESAMIENTO DATOS EMAs
+for df in dfs:
+    # a. Identificar datos faltantes y completar con NaN
+    dt = (df.timestart - df.timestart.shift(1)).value_counts().idxmax() # time delta
+    df = df.set_index('timestart')
+    serie = df[df.columns[-1]]
+    serie = serie.resample(dt).asfreq()
+
+    # b. Llevar a intensidad (en lugar de volumen)
+    serie = serie*pd.Timedelta(hours=1)/dt
+
+    # c. Llevar a intervalos de un minuto
+    serie_ = serie.resample('60s').bfill()
+
+    
+
