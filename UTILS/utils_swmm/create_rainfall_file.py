1c93c090f6b1a800dadbb94b69a49c1f435d7de7
marinalagos <mlmarinalagos@gmail.com>
2024-12-16 11:20:05 -0300

    Crear directorio si no existe, y redondear resultados a 2 decimales

--- UTILS/utils_swmm/create_rainfall_file.py
+++ UTILS/utils_swmm/create_rainfall_file.py
@@ -4,16 +4,25 @@
 # OUTPUT:
 # - Rainfall file con formato SWMM (STA01 2004 06 12 00 00 0.12). Es un único archivo con la info de todos los raingages.
 
+import os
+
 def create_rainfall_file(data, file_path):
     lines = []
 
+    columns = data.columns
+
     data['formatted_date'] = data.index.strftime("%Y\t%m\t%d\t%H\t%M")
 
-    for P in data.columns:
-        data['str'] = P + '\t' + data['formatted_date'] + '\t' + data[P].astype(str)
+    for P in columns:
+        print(data[P])
+        data['str'] = P + '\t' + data['formatted_date'] + '\t' + data[P].round(2).astype(str)
         lines = lines + list(data['str'])
 
     texto = '\n'.join(lines)
-    f = open (file_path,'w')
+
+    if not os.path.exists(file_path): 
+        os.makedirs(file_path) 
+
+    f = open(file_path + '/p.txt','w')
     f.write(texto)
     f.close()
