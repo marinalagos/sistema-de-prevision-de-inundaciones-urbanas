257bcbc8f55f558d43d4591019348177d1a69a96
marinalagos <mlmarinalagos@gmail.com>
2024-12-20 13:15:14 -0300

    borrar comillas de más

--- PAQUETES/swmm_ssd_emas_ina/bin/run_swmm.sh
+++ PAQUETES/swmm_ssd_emas_ina/bin/run_swmm.sh
@@ -6,4 +6,4 @@
 #SBATCH --time=192:00:00
 #SBATCH --nodelist=compute-0-[22-24]
 
-srun MODELS/swmm5_1-011-cluster data/HIST/PREP/2024/12/19/040000/swmm_ssd_emas_ina//model.inp data/HIST/ASIM/2024/12/19/040000/swmm_ssd_emas_ina//model.rpt data/HIST/ASIM/2024/12/19/040000/swmm_ssd_emas_ina//model.out"
+srun MODELS/swmm5_1-011-cluster data/HIST/PREP/2024/12/19/040000/swmm_ssd_emas_ina//model.inp data/HIST/ASIM/2024/12/19/040000/swmm_ssd_emas_ina//model.rpt data/HIST/ASIM/2024/12/19/040000/swmm_ssd_emas_ina//model.out
