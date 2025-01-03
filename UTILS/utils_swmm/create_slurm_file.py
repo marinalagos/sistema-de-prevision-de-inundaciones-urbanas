e4e644da673b870abdffc2b30dc95070b1e0a7c9
marinalagos <mlmarinalagos@gmail.com>
2025-01-03 11:15:33 -0300

    agregar funcion para crear archivo bash, sin comandos de slurm

--- UTILS/utils_swmm/create_slurm_file.py
+++ UTILS/utils_swmm/create_slurm_file.py
@@ -8,9 +8,18 @@ def create_slurm_file(path_slurm_file, path_swmm, pathdir_model, pathdir_out, jo
 #SBATCH --time=192:00:00
 #SBATCH --nodelist={nodelist}
 
-pwd
 srun {path_swmm} {pathdir_model}/model.inp {pathdir_out}/model.rpt {pathdir_out}/model.out
 """
 
     with open(path_slurm_file, "w") as file:
+        file.write(string)
+
+
+def create_bash_file(path_bash_file, path_swmm, pathdir_model, pathdir_out):
+    string = f"""#!/bin/bash
+
+./{path_swmm} {pathdir_model}/model.inp {pathdir_out}/model.rpt {pathdir_out}/model.out
+"""
+
+    with open(path_bash_file, "w") as file:
         file.write(string)
