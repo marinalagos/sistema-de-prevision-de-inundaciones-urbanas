09811bd294d745888b10c8670a95c0af0e2c7fa0
Marina Lagos <mlmarinalagos@gmail.com>
2025-04-09 14:38:59 -0300

    avances en la adecuación para este experimento. Falta revisar el paso 6

--- PAQUETES/swmm_ssd_wrf_ens_prono_10h/bin/preprocesamiento.py
+++ PAQUETES/swmm_ssd_wrf_ens_prono_10h/bin/preprocesamiento.py
@@ -35,6 +35,8 @@ repo_path = os.getenv('PYTHONPATH') # Obtener el directorio del repositorio desd
 if repo_path:
     os.chdir(repo_path)
 
+
+
 # 1. CONSULTAR ARCHIVOS .json DE PARÁMETROS
 with open(f'PAQUETES/{experimento}/config_exp/config.json', 'r') as f:
     content = f.read()
@@ -45,15 +47,21 @@ with open(f'PAQUETES/{experimento}/config_exp/config.json', 'r') as f:
 
 fin_sim = inicio_sim + pd.Timedelta(minutes = params['frecuencia_min'])
 
+
+
 # 2. CONSULTAR AWS
 df_coords, df_pp = consultar_aws(duracion_PHH = 10,
                                  duracion_PM  = 72,
                                  fecha_inicio = inicio_sim)
 
+
+
 # 3. GENERAR ARCHIVO DE PRECIPITACIÓN
 filepath_rainfall = f'data/HIST/PREP/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/p.txt'
 create_rainfall_file(data = df_pp,
                      file_path = filepath_rainfall)
+
+
 
 # 4. MODIFICAR .INP BASE (ASIGNACIÓN PLUVIO-SUBCUENCAS)
 modificar_inp_base(inp_file = params['inp_base'],
@@ -62,6 +70,8 @@ modificar_inp_base(inp_file = params['inp_base'],
                    epsg_SWMM = params['epsg_SWMM'],
                    epsg_precipitacion = params['epsg_precipitacion'])
 
+
+
 # 5. CONFIGURAR FECHAS Y PATHS CORRESPONDIENTES EN EL .INP
 # 5.a. Encontrar el hotstart de inicio, como el más reciente
 dirpath_lastest_hsf = find_dir_latest_file(root_dir = 'data/HIST/PREP/',
@@ -69,78 +79,34 @@ dirpath_lastest_hsf = find_dir_latest_file(root_dir = 'data/HIST/PREP/',
                                            file_name = 'hotstart.hsf')
 print(f'HSFin: {dirpath_lastest_hsf}')
 
-# 5.b. Generar .inp con las fechas y los paths correspondientes
+# 5.b. Crear directorio para recibir los archivos de salida (.rpt, .out, .hsf)
+dirpath_outputs = f'data/HIST/FCST/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/'
+if not os.path.exists(dirpath_outputs): 
+    os.makedirs(dirpath_outputs)
+
+# 5.c. Generar .inp con las fechas y los paths correspondientes
 setear_fechas_paths_inp(inicio_sim = inicio_sim,
                         fin_sim = fin_sim,
                         inp_base_modificado = params['inp_modificado'],
                         filepath_HSin = f'{dirpath_lastest_hsf}/hotstart.hsf',
-                        filepath_HSout = f'data/HIST/PREP/{fin_sim:%Y/%m/%d/%H%M%S}/{experimento}/hotstart.hsf',
+                        filepath_HSout = f'{dirpath_outputs}/hotstart.hsf', # Capaz no es necesario guardarlo
                         filepath_rainfall = filepath_rainfall,
                         dt_precipitacion_minutos = params['dt_precipitacion_minutos'],
-                        pformat = "VER",
+                        pformat = params['pformat'],
                         filepath_new_inp = f'data/HIST/PREP/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/model.inp'
                         )
 
-if False:
-    # 2. ASIGNACIÓN PLUVIO CUENCA
-    asignacion_pluvio_cuenca(inp_file = params['inp_base'],
-                            nc_file = params['nc_grid'], 
-                            inp_file_modificado = params['inp_modificado'],
-                            epsg_SWMM = params['epsg_SWMM'],
-                            epsg_precipitacion = params['epsg_precipitacion'],
-                            path_cell_coords = params['path_cell_coords'])
-
-    # 3. CONSULTAR EMAs BASE INA
-    if params['save_rainfall_raw_data']:
-        path_raw_data = f'data/HIST/OBS/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/p_base_ina.json'
-    else:
-        path_raw_data = False
-
-    grid_data = consultar_emas_base_ina(inicio_sim = inicio_sim,
-                                        fin_sim = fin_sim,
-                                        token_base_INA = token_base_INA,
-                                        params = params,
-                                        path_raw_data = path_raw_data)
-
-    # 4. GENERAR ARCHIVO DE PRECIPITACIÓN
-    create_rainfall_file(data = grid_data,
-                        file_path = f'data/HIST/PREP/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/p.txt')
-
-    # 5. GENERAR ARCHIVO .inp
-
-    # 5.a. Encontrar el hotstart de inicio, como el más reciente
-    pathdir_lastest_hsf = find_dir_latest_file(root_dir = 'data/HIST/PREP/',
-                                            experimento = 'swmm_ssd_emas_ina',
-                                            file_name = 'hotstart.hsf')
-    print(f'INPHSF:{pathdir_lastest_hsf}')
-
-    # 5.b. Crear archivo .inp
-    crear_inp(inicio_sim = inicio_sim,
-            fin_sim = fin_sim,
-            experimento = experimento,
-            inp_base = params['inp_modificado'],
-            pathdir_hsf = pathdir_lastest_hsf,
-            dt_precipitacion_minutos = params['dt_precipitacion_minutos'])
-
-    # 5.c. Crear la carpeta para el hotstart de salida y para los outputs
-    pathdir_out_hsf = f'data/HIST/PREP/{fin_sim:%Y/%m/%d/%H%M%S}/{experimento}/'
-    if not os.path.exists(pathdir_out_hsf): 
-        os.makedirs(pathdir_out_hsf)
-
-    pathdir_out_hsf = f'data/HIST/ASIM/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/'
-    if not os.path.exists(pathdir_out_hsf): 
-        os.makedirs(pathdir_out_hsf)
 
 
-    # 6. GENERAR ARCHIVO .sh
-    create_slurm_file(path_slurm_file = f'PAQUETES/{experimento}/bin/run_swmm_{inicio_sim:%Y%m%d%H%M}.sh',
-                    path_swmm = params['swmmexe'],
-                    pathdir_model = f'data/HIST/PREP/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/',
-                    pathdir_out = f'data/HIST/ASIM/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/',
-                    jobname = f'{experimento}_{inicio_sim:%Y%m%d%H%M}',
-                    # PENSAR SI HAY ALGUNA UBICACIÓN MEJOR PARA EL LOG
-                    logfile = f'data/HIST/ASIM/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/log.txt',
-                    nodelist = params['nodelist'],
-                    cpupertask = params['cpupertask'],
-                    errorfile = f'data/HIST/ASIM/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/error.txt'
-                    )
+# 6. GENERAR ARCHIVO .sh
+create_slurm_file(path_slurm_file = f'PAQUETES/{experimento}/bin/run_swmm_{inicio_sim:%Y%m%d%H%M}.sh',
+                  path_swmm = params['swmmexe'],
+                  pathdir_model = f'data/HIST/PREP/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/',
+                  pathdir_out = f'data/HIST/ASIM/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/',
+                  jobname = f'{experimento}_{inicio_sim:%Y%m%d%H%M}',
+                  # PENSAR SI HAY ALGUNA UBICACIÓN MEJOR PARA EL LOG
+                  logfile = f'data/HIST/ASIM/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/log.txt',
+                  nodelist = params['nodelist'],
+                  cpupertask = params['cpupertask'],
+                  errorfile = f'data/HIST/ASIM/{inicio_sim:%Y/%m/%d/%H%M%S}/{experimento}/error.txt'
+                  )
