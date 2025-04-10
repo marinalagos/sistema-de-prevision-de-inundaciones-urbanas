c2f5ee151506fda23274c5c1d61d193289aca0fd
Marina Lagos <mlmarinalagos@gmail.com>
2025-04-10 14:32:53 -0300

    cambio pathdir por dirpath

--- UTILS/utils_swmm/create_slurm_file.py
+++ UTILS/utils_swmm/create_slurm_file.py
@@ -1,8 +1,8 @@
-def create_slurm_file(path_slurm_file, path_swmm, pathdir_model, pathdir_out):
+def create_slurm_file(path_slurm_file, path_swmm, dirpath_model, dirpath_out):
     string = f"""#!/bin/bash
 
 pwd
-./{path_swmm} {pathdir_model}/model.inp {pathdir_out}/model.rpt {pathdir_out}/model.out
+./{path_swmm} {dirpath_model}/model.inp {dirpath_out}/model.rpt {dirpath_out}/model.out
 """
 
     with open(path_slurm_file, "w") as file:
