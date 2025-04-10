bede4760ca839ccea6b0e9fc0999900c10e0a669
Marina Lagos <mlmarinalagos@gmail.com>
2025-04-10 11:12:58 -0300

    codigo funcionando en el cluster, solo crea el bash para ejecutar swmm

--- UTILS/utils_swmm/create_slurm_file.py
+++ UTILS/utils_swmm/create_slurm_file.py
@@ -1,25 +1,9 @@
 def create_slurm_file(path_slurm_file, path_swmm, pathdir_model, pathdir_out, jobname, logfile, nodelist, cpupertask, errorfile):
     string = f"""#!/bin/bash
-#
-#SBATCH --cpus-per-task={cpupertask}
-#SBATCH --job-name={jobname}
-#SBATCH --output={logfile}
-#SBATCH --error={errorfile}
-#SBATCH --time=192:00:00
-#SBATCH --nodelist={nodelist}
 
-srun {path_swmm} {pathdir_model}/model.inp {pathdir_out}/model.rpt {pathdir_out}/model.out
-"""
-
-    with open(path_slurm_file, "w") as file:
-        file.write(string)
-
-
-def create_bash_file(path_bash_file, path_swmm, pathdir_model, pathdir_out):
-    string = f"""#!/bin/bash
-
+pwd
 ./{path_swmm} {pathdir_model}/model.inp {pathdir_out}/model.rpt {pathdir_out}/model.out
 """
 
-    with open(path_bash_file, "w") as file:
+    with open(path_slurm_file, "w") as file:
         file.write(string)
